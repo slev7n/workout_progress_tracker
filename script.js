8fd20427078683527cdb468f2f2401d546f1fa84
slev7n <slev7n@gmail.com>
2018-05-30 16:50:36 +0200

    'lorem'

--- script.js
+++ script.js
@@ -1,58 +1,95 @@
-let addSet = document.querySelector('#addSet');
-let totalsDiv = document.querySelector('#total');
-init();
 
 
-function init() {
+function Workout(parent) {
+	let parentEl = parent || document.body;
+	let setCount = 1;
+
+	let wrapper = document.createElement('div');
+		wrapper.setAttribute('class', 'workout');
+
+	let title = document.createElement('div');
+		title.setAttribute('contenteditable', 'true');;
+		title.setAttribute('class', 'title');
+
+	let total = document.createElement('div');
+		total.setAttribute('class', 'total');
+		total.innerText = 0;
+
+	let addSet = document.createElement('button');
+		addSet.setAttribute('class', 'add-set');
+		addSet.innerText = '+';
+
+	let storage = {
+		set: function() {
+			let arr = [];
+			let obj = {};
+				obj.title = '';
+				obj.reps = [];
+			document.querySelectorAll('.workout').forEach(function(workout) {
+				obj.title = workout.querySelector('.title').innerText;
+				workout.querySelectorAll('.reps').forEach(function(rep) {
+					obj.reps.unshift(parseInt(rep.value));
+				});
+				arr.unshift(obj);
+				localStorage['workout'] = JSON.stringify(arr);
+			});
+		},
+		get: function() {
+			return JSON.parse(localStorage['workout']);
+		}
+	}
+
+	let makeTotal = function() {
+		let totalCount = 0;
+		let obj_arr = [];
+		this.parentElement.querySelectorAll('.reps').forEach(function(el) {
+			let value = parseInt(el.value);
+			if(Number.isInteger(value)) {
+				totalCount += value;
+				obj_arr.unshift({reps: value});
+			}
+		});
+		this.parentElement.querySelector('.total').innerText = totalCount;
+		storage.set();
+	}
+
+	let generate = function() {
+		let set = document.createElement('div');
+			set.setAttribute('class', 'sets');
+			set.innerText = setCount++;
+
+		let rep = document.createElement('input');
+			rep.setAttribute('type', 'text');
+			rep.setAttribute('class', 'reps');
+			rep.addEventListener('keyup', makeTotal);
+		this.parentElement.insertBefore(rep, this.nextElementSibling);
+		this.parentElement.insertBefore(set, rep);
+	}
+
+	wrapper.appendChild(title);
+	wrapper.appendChild(total);
+	wrapper.appendChild(addSet);
+	parentEl.appendChild(wrapper);
+
 	try {
 		let arr = JSON.parse(localStorage['workout']);
-		this.setCount = 1;
-		for(let el of arr) {
-			generate.call(addSet, el.reps);
-		}
-		totalsDiv.innerText = arr.reduce(function(a,b) {
-			a += parseInt(b.reps);
-			return a;
-		}, 0);
-		this.setCount = arr.length;
+
 	} catch(e) {
 		console.log(e.message);
-		this.setCount = 1;
 		generate.call(addSet);
+		addSet.addEventListener('click', generate);
 	}
 }
 
+new Workout();
+new Workout();
+new Workout();
 
-document.querySelector('.reps').addEventListener('keyup', makeTotal);
 
-function makeTotal() {
-	let total = 0;
-	let obj_arr = [];
-	document.querySelectorAll('.reps').forEach(function(el) {
-		let value = parseInt(el.value);
-		if(Number.isInteger(value)) {
-			total += value;
-			obj_arr.unshift({reps: value});
-		}
-	});
-	totalsDiv.innerText = total;
-	localStorage['workout'] = JSON.stringify(obj_arr);
-}
 
-function generate(repValue) {
-	let set = document.createElement('input');
-		set.setAttribute('type', 'text');
-		set.setAttribute('disabled', 'true');
-		set.setAttribute('class', 'sets');
-		set.setAttribute('placeholder', setCount++);
-	let rep = document.createElement('input');
-		rep.setAttribute('type', 'text');
-		rep.setAttribute('class', 'reps');
-		rep.addEventListener('keyup', makeTotal);
-		if(repValue && !repValue.target)
-			rep.setAttribute('value', repValue)
-	this.parentElement.insertBefore(rep, this.nextElementSibling);
-	this.parentElement.insertBefore(set, rep);
-}
 
-addSet.addEventListener('click', generate);
+
+
+
+
+
